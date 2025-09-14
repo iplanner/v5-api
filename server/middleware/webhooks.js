7e71c9396328559973f2177c2d08b47fae758444
helix109 <helmut.leier@i-planner.de>
2025-09-14 20:01:45 +0200

    update webhook

--- server/middleware/webhooks.js
+++ server/middleware/webhooks.js
@@ -1,38 +1,36 @@
-
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig(event);
-  const { pathname } = getRequestURL(event);
+  const config = useRuntimeConfig(event)
+  const { pathname } = getRequestURL(event)
 
   // Nur Webhook-Routen prüfen
-  const allowedRoutes = ["/webhooks"]
-  if (!allowedRoutes.some(route => pathname.startsWith(route))) {
-    return
-  }
+  if (!pathname.startsWith('/webhooks')) return
 
-  // IP prüfen ---
-   const allowIps = (config.EE_WEBHOCK_ALLOWED_IPS || '').split(',')
-  .map(s => s.trim())
-  .filter(Boolean);
+  // IP prüfen
+  const allowIps = (config.EE_WEBHOCK_ALLOWED_IPS || '')
+    .split(',')
+    .map(s => s.trim())
+    .filter(Boolean)
 
   const xff = getHeader(event, 'x-forwarded-for') || ''
   const clientIp = (xff.split(',')[0] || event.node.req.socket.remoteAddress || '').trim()
 
-  console.log('allowIps',allowIps)
-  console.log('clientIp',clientIp)
-
-  if (allowIps.length && clientIp && !isIpAllowed(clientIp, allowIps)) {
-    return sendError(event, createError({ statusCode: 403, statusMessage: 'Forbidden: IP not allowed' }))
+  if (
+    allowIps.length &&
+    clientIp &&
+    !allowIps.some(ip => ip === clientIp)
+  ) {
+    return sendError(
+      event,
+      createError({ statusCode: 403, statusMessage: 'Forbidden: IP not allowed' })
+    )
   }
 
-  // X-API-KEY prüfen 
-  const apiKey = getHeader(event, "x-api-key")
-  if (apiKey !== config["X_API_KEY"]) {
-    return sendError( event, createError({
-      statusCode: 403,
-      statusMessage: "Forbidden: Invalid API Key",
-    }))
+  // X-API-KEY prüfen
+  const apiKey = getHeader(event, 'x-api-key')
+  if (apiKey !== config.X_API_KEY) {
+    return sendError(
+      event,
+      createError({ statusCode: 403, statusMessage: 'Forbidden: Invalid API Key' })
+    )
   }
-
-
-
-});
+})
