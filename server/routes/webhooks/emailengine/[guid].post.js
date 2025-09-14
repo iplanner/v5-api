0a6f990cea6f09192a10cafac5a55d7619dfd8d8
helix109 <helmut.leier@i-planner.de>
2025-09-14 19:38:40 +0200

    update webhook

--- server/routes/webhooks/emailengine/[guid].post.js
+++ server/routes/webhooks/emailengine/[guid].post.js
@@ -1,11 +1,14 @@
 export default defineEventHandler(async (event) => {
+  
   const config = useRuntimeConfig(event);
 
   const guid = getRouterParam(event, 'guid');
-  let body = await readBody(event);
+  const body = await readBody(event);
+  const header = getHeader(event, "x-api-key")
 
   console.log('guid',guid);
   console.log('body',body);
+  console.log('header',header);
 
   return guid;
 
